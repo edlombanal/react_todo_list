547989db819241c29d3a9d4b900b79015380218f
edlombanal <edlombanal@gmail.com>
2023-07-09 18:01:28 -0500

    Create card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.scss';
+import "./App.scss";
+import Card from "./Card.jsx";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello! Welcome to my to do list project.</h1>
       <p>Initial bootstrap test.</p>
+      <div className="container-lg">
+        <Card />
+      </div>
     </div>
   );
 }
