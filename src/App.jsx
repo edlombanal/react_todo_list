e026ad9d5f4a46d0fba5aabbd25ee82606710399
edlombanal <edlombanal@gmail.com>
2023-07-09 17:42:32 -0500

    Adjust file extensions and test bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello! Welcome to my to do list project.</h1>
+      <p>Initial bootstrap test.</p>
+    </div>
+  );
+}
+
+export default App;
