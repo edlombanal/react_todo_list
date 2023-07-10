b1f6bc700befee4cb0db3d0aa60c41748494fbc2
edlombanal <edlombanal@gmail.com>
2023-07-10 18:59:55 -0500

    Create todo list skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.scss";
 import Counter from "./Pages/Counter/Counter.jsx";
+import ToDoList from "./Pages/ToDoList/ToDoList";
 
 function App() {
   return (
     <div className="App">
       <h1 className="bg-black text-white">To do list and counter project.</h1>
       <div className="container-lg">
-        <Counter />
+        <ToDoList />
       </div>
     </div>
   );
